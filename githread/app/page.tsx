7e8082e56f68287463b3f3716470e7456f4d618b
Mylan699 <mylan.mahieddine@ecole-isitech.fr>
2024-01-16 11:16:30 +0100

    App initialization

---
+++ githread/app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from '@/components/ui/button'
+import { Input } from '@/components/ui/input'
+import Image from 'next/image'
+
+export default function Home() {
+  return (
+    <div className='m-6'>
+      <Button className='m-4'>Click Me</Button>
+      <Input/>
+    </div>
+  )
+}
